544bdc5b4eb5159afaf13013b868e6d1dbc7f0a6
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-11-26 12:23:34 +0530

    inital commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        id : '1',
+        question: "what are accordion components?",
+        answer:
+        "Accodion components are user interface"
+    },
+    {
+        id : '2',
+        question: "what they are used for?",
+        answer:
+        "Accodion components are user interface"
+    },
+    {
+        id : '3',
+        question: "Accordin as a musical instrument?",
+        answer:
+        "Accodion components are user interface"
+    },
+    {
+        id : '4',
+        question: "can i create an accordion component with different forms?",
+        answer:
+        "Accodion components are user interface"
+    },
+];
+export default data;
